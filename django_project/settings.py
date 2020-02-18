fda2311d1f254660aac278270a43f552727fdfba
sang0926 <sl60486@uga.edu>
2020-02-18 14:15:24 -0500

    Updated Settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False   
 
-ALLOWED_HOSTS = ['mydjangoblogapp01.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['mydjangoblogapp01.herokuapp.com']
 
 
 # Application definition
