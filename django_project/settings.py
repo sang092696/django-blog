ceea1edb8404999dd40343d29ade90f34bbc6779
sang0926 <sl60486@uga.edu>
2020-02-18 19:03:56 -0500

    update email env

--- django_project/settings.py
+++ django_project/settings.py
@@ -137,8 +137,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('USER_EMAIL')
-EMAIL_HOST_PASSWORD = os.environ.get('USER_PASSWORD')
+EMAIL_HOST_USER = 'sang0926@gmail.com'
+EMAIL_HOST_PASSWORD = 'iamapastorskid96'
 
 AWS_ACCESS_KEY_ID= os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY= os.environ.get('AWS_SECRET_ACCESS_KEY')
