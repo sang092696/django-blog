53f7261d711a7498b732ca9c127dc61ac1ab3288
sang0926 <sl60486@uga.edu>
2020-02-19 19:36:43 -0500

    Fixed indent in urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
 from . import views
-from .views import PostListView, 
-                   PostDetailView, 
-                   PostCreateView, 
-                   PostUpdateView, 
-                   PostDeleteView, 
-                   UserPostListView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'), #looks for <app>/<model>_<viewtype>.html
